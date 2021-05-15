032834787789e7b618bffdb7180bca1b86ea8b3e
v-Madhusudan Patidar <v-mpatidar@avtex.com>
2021-05-15 15:32:15 +0530

    selenium

--- SeleniumTest/UnitTest1.cs
+++ SeleniumTest/UnitTest1.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
 
 namespace SeleniumTest
 {
@@ -9,7 +12,33 @@ namespace SeleniumTest
         [TestMethod]
         public void TestMethod1()
         {
+            ChromeOptions CHoptions = new ChromeOptions();
+            var driver = new ChromeDriver(CHoptions);
+            //CHoptions.AddArguments("headless");
 
+
+            //Notice navigation is slightly different than the Java version
+            //This is because 'get' is a keyword in C#
+            driver.Navigate().GoToUrl("http://www.google.com/");
+
+            // Find the text input element by its name
+            IWebElement query = driver.FindElement(By.Name("q"));
+
+            // Enter something to search for
+            query.SendKeys("Cheese");
+
+            // Now submit the form. WebDriver will find the form for us from the element
+            query.Submit();
+
+            // Google's search is rendered dynamically with JavaScript.
+            // Wait for the page to load, timeout after 10 seconds
+            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            wait.Until(d => d.Title.StartsWith("cheese", StringComparison.OrdinalIgnoreCase));
+
+            // Should see: "Cheese - Google Search" (for an English locale)
+            Assert.AreEqual(driver.Title, "Cheese - Google Search");
+
+            driver.Quit();
 
         }
     }
